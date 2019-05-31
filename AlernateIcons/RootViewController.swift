a71fb97dddd7c04bc435733c1af4f53d433b5764
Alex <therebeloper@gmail.com>
2019-05-31 09:45:44 +0300

    Initial Commit

---
+++ AlernateIcons/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  AlernateIcons
+//
+//  Created by Alex Nagy on 31/05/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
